e4056add70f2b18c0098d540c08eea481ab39b51
Bruno Duarte <brunoarduarte@gmail.com>
2021-12-22 19:47:41 -0300

    Implementando AutoCloseable

--- src/Conta.java
+++ src/Conta.java
@@ -1,5 +1,16 @@
 public class Conta {
 
+    public Conta(int agencia, int numero){
+
+        if(agencia < 1) {
+            throw new IllegalArgumentException("Agencia inválida");
+        }
+
+        if(numero < 1) {
+            throw new IllegalArgumentException("Numero da conta inválido");
+        }
+    }
+
     void deposita() throws MinhaExcecao2 {
 
     }
--- src/TestaContaComExcecaoChecked.java
+++ src/TestaContaComExcecaoChecked.java
@@ -1,7 +1,7 @@
 public class TestaContaComExcecaoChecked {
 
     public static void main(String[] args) {
-        Conta c = new Conta();
+        Conta c = new Conta(123, 321);
         try {
             c.deposita();
         } catch (MinhaExcecao2 ex) {
